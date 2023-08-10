952d0008e5fcfd9966fb24868be297c3fb7c3bfa
psp2111-ADSLGATE <80945174+psp2111-ADSLGATE@users.noreply.github.com>
2023-08-10 18:01:01 +0300

    Update addons

--- repo/script.module.acctview/default.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    from resources.libs.common import router
-
-    _handle = int(sys.argv[1])
-    _params = sys.argv[2][1:]
-    
-    dispatcher = router.Router()
-    dispatcher.dispatch(_handle, _params)
